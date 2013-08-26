df1861faea9b2cc3e5f94ed54963c4038c7106d9
LoveJenny <343875957@qq.com>
2013-08-26 15:40:35 +0800

    Add Model

---
+++ EasyRight/EasyRight/Models/ERRightManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyRight.Models
+{
+    public class ERRightManager
+    {
+        private ERRightManager()
+        { }
+
+        public static ERRightManager _instance = new ERRightManager();
+
+        public static ERRightManager Instance
+        {
+            get { return _instance; }
+        }
+
+        public string ERUserDefinition
+        {
+            get;
+            set;
+        }
+
+        public string ERRoleDefinition
+        {
+            get;
+            set;
+        }
+
+        public string EROptionDefinition
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
---
+++ EasyRight/EasyRight/Models/ERUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyRight.Models
+{
+    public class ERUser
+    {
+        public Guid Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public dynamic Properties
+        {
+            get;
+            set;
+        }
+    }
+}
