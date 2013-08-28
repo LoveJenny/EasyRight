a393f1f13f0cf0c6babc8d6e4823c9b7ac218e31
LoveJenny <343875957@qq.com>
2013-08-28 21:04:03 +0800

    add datetime editor template.

--- EasyRight/EasyRight/Models/ERUser.cs
+++ EasyRight/EasyRight/Models/ERUser.cs
@@ -8,24 +8,35 @@ namespace EasyRight.Models
 {
     public class ERUser
     {
+        [UIHint("HiddenInput")]
         public Guid Id { get; set; }
 
         public string UserNumber { get; set; }
 
         public string Name { get; set; }
 
+        [UIHint("EmailAddress")]
         public string Email { get; set; }
 
+        [UIHint("Password")]
         public string Password { get; set; }
 
         public bool IsEnable { get; set; }
 
-        private Dictionary<string, object> propertiesData = new Dictionary<string, object>();
-        public Dictionary<string, object> Properties
+        private Dictionary<string, string> propertiesData = new Dictionary<string, string>();
+
+        [UIHint("Collection")]
+        public Dictionary<string, string> Properties
         {
             get { return this.propertiesData; }
             set { this.propertiesData = value; }
         }
 
+        [UIHint("DatePicker")]
+        public DateTime DatePicker
+        {
+            get;
+            set;
+        }
     }
 }
